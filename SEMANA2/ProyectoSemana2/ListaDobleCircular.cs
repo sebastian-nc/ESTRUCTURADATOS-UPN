dcb69c1b79a0c53a877ab596a7e88830ff004d7b
sebastian <scsebastian1406@gmail.com>
2026-04-10 11:50:53 -0500

    Metodos LIFO, eliminar - ordenar no implementado

--- SEMANA2/ProyectoSemana2/ListaDobleCircular.cs
+++ SEMANA2/ProyectoSemana2/ListaDobleCircular.cs
@@ -36,9 +36,42 @@ namespace ProyectoSemana2
             }
 
         }
+        
+        public void InsertarLIFO(int dato)
+        {
+            nuevo = new NodoCircularDoble();
+            nuevo.info = dato;
+            ptr = aux = raiz;
+
+            if (raiz == null)
+            {
+                raiz = nuevo;
+                raiz.sig = raiz.ant = nuevo;
+            }
+            else
+            {
+                do
+                {
+                    aux = ptr;
+                    ptr = ptr.sig;
+                }
+                while (ptr != raiz);
+
+                raiz.ant = nuevo;
+                nuevo.sig = raiz;
+                nuevo.ant = aux;
+                aux.sig = nuevo;
+                raiz = nuevo;
+            }
+        }
 
         public void Recorrido()
         {
+            if (raiz == null)
+            {
+                Console.WriteLine("LISTA VACIA");
+                return;
+            }
             ptr = raiz;
 
             do
@@ -47,6 +80,75 @@ namespace ProyectoSemana2
                 ptr = ptr.sig;
             }
             while (ptr != raiz);
+        }
+        
+        public bool Buscar(int dato)
+        {
+            bool encontrado = false;
+            ptr = raiz;
+
+            do
+            {
+                if (ptr.info == dato)
+                {
+                    encontrado = true;
+                }
+
+                ptr = ptr.sig;
+            }
+            while (ptr != raiz);
+
+            return encontrado;
+        }
+
+        public bool Eliminar(int dato) {
+            bool encontrado = false;
+            ptr = aux = raiz;
+
+            if (raiz == null) return encontrado;
+
+            // Inicio
+            if (raiz.info == dato)
+            {
+                if (raiz.sig == raiz)
+                {
+                    raiz = null;
+                }else
+                {
+                    raiz.sig.ant = raiz.ant;
+                    raiz = raiz.sig;
+                    raiz.ant.sig = raiz;
+                }
+                encontrado = true;
+            }
+            // Ultimo
+            else if (raiz.ant.info == dato)
+            {
+                raiz.ant.ant.sig = raiz;
+                raiz.ant = raiz.ant.ant;
+                encontrado = true;
+            } else
+            {
+                do
+                {
+                    if (ptr.info == dato)
+                    {
+                        encontrado = true;
+                    } else
+                    {
+                        ptr = ptr.sig;
+                    }
+                }
+                while (ptr != raiz && !encontrado);
+
+                if (encontrado)
+                {
+                    ptr.ant.sig = ptr.sig;
+                    ptr.sig.ant = ptr.ant;
+                }
+            }
+           
+            return encontrado;
         }
     }
 }
